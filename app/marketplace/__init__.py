2c608aaca483be4a97cf568266ea6c952d7b4d20
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-09-25 23:25:37 +0600

    feat: export marketplace accept action

--- app/marketplace/__init__.py
+++ app/marketplace/__init__.py
@@ -1,10 +1,12 @@
 from app.marketplace.service import (
+    accept_marketplace_order,
     get_marketplace_order,
     get_marketplace_summary,
     list_marketplace_orders,
 )
 
 __all__ = [
+    "accept_marketplace_order",
     "get_marketplace_order",
     "get_marketplace_summary",
     "list_marketplace_orders",
