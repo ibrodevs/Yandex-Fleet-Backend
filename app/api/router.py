f2e37f621acc26d90fba5fcf5b6ae1f7539d25fa
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-09-25 22:36:57 +0600

    feat: register Mini App API

--- app/api/router.py
+++ app/api/router.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from app.api.v1.drivers import router as drivers_router
 from app.api.v1.health import router as health_router
 from app.api.v1.integrations import router as integrations_router
+from app.api.v1.miniapp import router as miniapp_router
 from app.api.v1.orders import router as orders_router
 from app.api.v1.telegram import router as telegram_router
 
@@ -11,5 +12,6 @@ api_router.include_router(health_router)
 api_router.include_router(drivers_router)
 api_router.include_router(orders_router)
 api_router.include_router(integrations_router)
+api_router.include_router(miniapp_router)
 
 api_router.include_router(telegram_router)
