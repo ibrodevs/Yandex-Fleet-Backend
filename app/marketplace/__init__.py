285beff4c2dd10c78b1f31ac014426c54d36441c
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-09-25 22:33:49 +0600

    feat: add multi-aggregator mock marketplace

---
+++ app/marketplace/__init__.py
@@ -0,0 +1,11 @@
+from app.marketplace.service import (
+    get_marketplace_order,
+    get_marketplace_summary,
+    list_marketplace_orders,
+)
+
+__all__ = [
+    "get_marketplace_order",
+    "get_marketplace_summary",
+    "list_marketplace_orders",
+]
